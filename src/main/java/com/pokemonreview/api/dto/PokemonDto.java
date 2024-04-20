222b94c30ece471c00997257951c54ec14e14509
ritwikgs <ritwik.gs@gmail.com>
2024-04-20 13:47:15 +0530

    refactor/removed-unused-imports

--- src/main/java/com/pokemonreview/api/dto/PokemonDto.java
+++ src/main/java/com/pokemonreview/api/dto/PokemonDto.java
@@ -1,8 +1,5 @@
 package com.pokemonreview.api.dto;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
